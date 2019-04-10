4542ebdbdbc8d12db03b062a388749db9b85bb84
Hussain Abbas <habbas@keyhole.co>
2019-04-10 17:23:03 -0400

    Rebuild dist to include latest src changes.

--- dist/microbundle.js
+++ dist/microbundle.js
@@ -549,7 +549,7 @@ function createConfig(options, entry, format, writeMeta) {
         })].filter(Boolean),
         // only write out CSS for the first bundle (avoids pointless extra files):
         inject: !!options.injectStyles,
-        extract: !!writeMeta
+        extract: !options.injectStyles && !!writeMeta
       }), Object.keys(moduleAliases).length > 0 && alias(Object.assign({}, moduleAliases, {
         resolve: EXTENSIONS
       })), nodeResolve({
